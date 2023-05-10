175c968000b435e6b16b74094b646da5ed2c380e
Valene-R <valene.renaud@gmail.com>
2023-05-10 21:31:16 +0200

    Get categories and works. Add login page

---
+++ FrontEnd/script.js
@@ -0,0 +1,60 @@
+//const worksGallery = document.querySelector(".gallery");
+
+//fetch("http://localhost:5678/api/works")
+//    .then(response => response.json())
+//    .then(gallery => {
+
+//        gallery.forEach(element => {
+//            const works = document.createElement("div");
+//            works.innerHTML = `<div>
+//            <figure><img src="${element.imageUrl}"><figcaption>${element.title}</figcaption></figure>
+//            </div>`
+            
+//            worksGallery.appendChild(works)
+
+//        });
+//    });
+
+//// sélectionner l'élément HTML ".categories" 
+//const projectsCategories = document.querySelector(".categories");
+
+// récupérer les catégories depuis l'API
+//fetch("http://localhost:5678/api/categories")
+//   .then(response => response.json())
+//   .then(categories => {
+    
+//      console.log(categories);
+
+        // ajouter un bouton "tous" dans projectsCategories
+//        const allCategories = document.createElement("button");
+//        allCategories.innerHTML = `Tous`
+//        projectsCategories.appendChild(allCategories)
+
+        // boucle sur categories
+//        categories.forEach(element => {
+
+            // ajouter l'élément dans projectsCategories
+//            const projects = document.createElement("button");
+//            projects.innerHTML = `${element.name}`
+         
+//            projectsCategories.appendChild(projects)
+
+            // ajouter un event "click" à chaque bouton
+ //           projectsCategories.addEventListener("click", () => {
+//            console.log((element.name)
+
+
+
+        //    projects.addEventListener("click", () => {
+        //        console.log(element.name);
+                //  Appeler la fonction de filtrage avec la catégorie sélectionnée
+         //       filterProjectsByCategory(element.name);   
+//            )});
+//      });
+//  });
+
+
+
+
+
+
